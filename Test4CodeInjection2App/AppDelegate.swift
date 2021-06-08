f65d32973ac7ea2d696f35008aaffc3c55458d69
jetedonner <kim@kimhauser.ch>
2021-06-08 18:37:13 +0200

    Initial Commit

---
+++ Test4CodeInjection2App/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Test4CodeInjection2App
+//
+//  Created by Kim David Hauser on 08.06.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Test4CodeInjection2App/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Test4CodeInjection2App
+//
+//  Created by Kim David Hauser on 08.06.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
